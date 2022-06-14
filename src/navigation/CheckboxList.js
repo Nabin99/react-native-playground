8adc5fc3443320a2adff574bac651c4f5a964843
Nabin Dhital <dhitalnabin11@gmail.com>
2022-06-14 13:20:47 +0545

    used global flex styles in checkboxcomponet

--- src/navigation/CheckboxList.js
+++ src/navigation/CheckboxList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import CheckBoxComponent from '../component/CheckBoxComponent';
 import ScrollViewContainer from '../containers/ScrollViewContainer';
-import {stylesConstant} from '../styles/abstracts/abstracts';
 import stylesProvider from '../styles/styles';
 
 const CheckboxList = () => {
@@ -21,8 +20,6 @@ const styles = StyleSheet.create({
   checkBoxWrapper: {
     marginTop: 100,
     width: '100%',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    ...stylesProvider.flexStyles(),
   },
 });
