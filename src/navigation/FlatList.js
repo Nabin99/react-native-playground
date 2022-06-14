4b5e1c1f6c4689c14d5331210130e99c814bd9bf
Nabin Dhital <dhitalnabin11@gmail.com>
2022-06-14 15:24:38 +0545

    Added flatlist navigation

--- src/navigation/FlatList.js
+++ src/navigation/FlatList.js
@@ -1,11 +1,67 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+import FlatListContainer from '../containers/FlatListContainer';
+
+import ComponentCard from '../component/ComponentCard';
+import textImage from '../assets/texts.jpg';
 
 const FlatList = () => {
   return (
-    <View>
-      <Text>This is example</Text>
-    </View>
+    <>
+      <FlatListContainer
+        cols={2}
+        dataList={[
+          {id: 1, name: 'AlertList', image: textImage},
+          {id: 2, name: 'AvatarList', image: textImage},
+          {id: 3, name: 'ButtonList', image: textImage},
+          {id: 4, name: 'CheckboxList', image: textImage},
+          {id: 5, name: 'DividerList', image: textImage},
+          {id: 6, name: 'FlatList', image: textImage},
+          {id: 7, name: 'IconList', image: textImage},
+          {id: 8, name: 'ImageList', image: textImage},
+          {id: 9, name: 'InputList', image: textImage},
+          {id: 10, name: 'ModalList', image: textImage},
+          {id: 11, name: 'SwitchList', image: textImage},
+          {id: 12, name: 'TextareaList', image: textImage},
+          {id: 13, name: 'TextList', image: textImage},
+          {id: 14, name: 'ToastList', image: textImage},
+        ]}
+        renderItem={({item}) => (
+          <ComponentCard
+            text={item.name}
+            image={item.image}
+            clickHandler={() => null}
+          />
+        )}
+      />
+      <Text>---------------------------------</Text>
+      <FlatListContainer
+        cols={1}
+        dataList={[
+          {id: 1, name: 'AlertList', image: textImage},
+          {id: 2, name: 'AvatarList', image: textImage},
+          {id: 3, name: 'ButtonList', image: textImage},
+          {id: 4, name: 'CheckboxList', image: textImage},
+          {id: 5, name: 'DividerList', image: textImage},
+          {id: 6, name: 'FlatList', image: textImage},
+          {id: 7, name: 'IconList', image: textImage},
+          {id: 8, name: 'ImageList', image: textImage},
+          {id: 9, name: 'InputList', image: textImage},
+          {id: 10, name: 'ModalList', image: textImage},
+          {id: 11, name: 'SwitchList', image: textImage},
+          {id: 12, name: 'TextareaList', image: textImage},
+          {id: 13, name: 'TextList', image: textImage},
+          {id: 14, name: 'ToastList', image: textImage},
+        ]}
+        renderItem={({item}) => (
+          <ComponentCard
+            text={item.name}
+            clickHandler={() => null}
+            image={item.image}
+          />
+        )}
+      />
+    </>
   );
 };
 
--- src/navigation/Home.js
+++ src/navigation/Home.js
@@ -3,9 +3,6 @@ import {SafeAreaView} from 'react-native';
 import ComponentCard from '../component/ComponentCard';
 import FlatListContainer from '../containers/FlatListContainer';
 import textImage from '../assets/texts.jpg';
-import buttonImage from '../assets/buttons.png';
-import toastImage from '../assets/Toast.jpeg';
-import textareaImage from '../assets/textarea.png';
 
 const Home = ({navigation}) => {
   return (
@@ -15,18 +12,18 @@ const Home = ({navigation}) => {
         dataList={[
           {id: 1, name: 'AlertList', image: textImage},
           {id: 2, name: 'AvatarList', image: textImage},
-          {id: 3, name: 'ButtonList', image: buttonImage},
+          {id: 3, name: 'ButtonList', image: textImage},
           {id: 4, name: 'CheckboxList', image: textImage},
           {id: 5, name: 'DividerList', image: textImage},
           {id: 6, name: 'FlatList', image: textImage},
           {id: 7, name: 'IconList', image: textImage},
           {id: 8, name: 'ImageList', image: textImage},
           {id: 9, name: 'InputList', image: textImage},
-          {id: 11, name: 'ModalList', image: textImage},
+          {id: 10, name: 'ModalList', image: textImage},
           {id: 11, name: 'SwitchList', image: textImage},
-          {id: 12, name: 'TextareaList', image: textareaImage},
+          {id: 12, name: 'TextareaList', image: textImage},
           {id: 13, name: 'TextList', image: textImage},
-          {id: 14, name: 'ToastList', image: toastImage},
+          {id: 14, name: 'ToastList', image: textImage},
         ]}
         renderItem={({item}) => (
           <ComponentCard
