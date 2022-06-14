bb8e8b909bf3dc3637e3a3dc7d90b2d90b09662e
Nabin Dhital <dhitalnabin11@gmail.com>
2022-06-14 10:42:44 +0545

    Added Avatar Component

--- src/navigation/AvatarList.js
+++ src/navigation/AvatarList.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import Avatar from '../component/AvatarComponent';
+import ScrollViewContainer from '../containers/ScrollViewContainer';
+import profileImage from '../assets/profile.jpg';
+import {stylesConstant} from '../styles/abstracts/abstracts';
 
 const AvatarList = () => {
   return (
-    <View>
-      <Text>This is example</Text>
-    </View>
+    <ScrollViewContainer>
+      <Avatar
+        Img={profileImage}
+        borderRadius={100}
+        borderWidth={8}
+        borderColor={stylesConstant.color.primaryColor}
+      />
+      <Avatar
+        Img={profileImage}
+        borderRadius={100}
+        borderWidth={1}
+        borderColor={stylesConstant.color.primaryColor}
+      />
+      <Avatar
+        Img={profileImage}
+        borderRadius={10}
+        borderWidth={8}
+        borderColor={stylesConstant.color.primaryColor}
+      />
+    </ScrollViewContainer>
   );
 };
 
